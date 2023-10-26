80d6c40edffd5fba203c9d97e06abb97b0dbe4c2
Matthew <matthew_huynh09@yahoo.com>
2023-10-26 18:13:27 -0400

    refactor code

--- handTracker.py
+++ handTracker.py
@@ -1,66 +1,64 @@
 import cv2
 import mediapipe as mp
-import slider as s 
+import sys
 from collections import deque
+# import slider as s 
 
-def midpoint(x_pos, y_pos):
+class PaddleTracker(object):
+    def __init__(self, window_name = "main") -> None:
+        self.window_name = window_name 
+        self.delay = 20
+        self.coords = deque(maxlen=self.delay)
+        self.cap = cv2.VideoCapture(0)
+        self.mp_hands = mp.solutions.hands
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.hands = self.mp_hands.Hands()
+        cv2.namedWindow(self.window_name)
 
-    c_x_pos = sum(x_pos)/len(x_pos)
-    c_y_pos = sum(y_pos)/len(y_pos)
+    def midpoint(self, x_pos, y_pos):
+        c_x_pos = sum(x_pos)/len(x_pos)
+        c_y_pos = sum(y_pos)/len(y_pos)
+        center = [c_x_pos,c_y_pos]    
+        return center
 
-    center = [c_x_pos,c_y_pos]    
+    def track(self):
+        while True:
+            img = self.cap.read()[1]
 
-    return center
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = self.hands.process(img)
 
-cap = cv2.VideoCapture(0)
+            #use BGR not RGB
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
+            image_height, image_width, _ = img.shape
 
-hands = mp_hands.Hands()
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    hand_pos = hand_landmarks.landmark
+                    x_pos = [hand_pos[0].x, hand_pos[5].x, hand_pos[17].x]
+                    y_pos = [hand_pos[0].y,hand_pos[5].y,hand_pos[17].y]                
+                        
+                    c = self.midpoint(x_pos, y_pos)
+                    cx = int(c[0]*image_width)
+                    cy = int(c[1]*image_height)
 
-webcam=cv2.VideoCapture(0)
+                    self.coords.appendleft([cx,cy])
+                    cv2.circle(img,(cx,cy),1,(255, 0, 0),3)
+                    
+                    for i in range(1, len(self.coords)):
+                        if self.coords[i-1] == None or self.coords[i] == None:
+                            continue
+                        cv2.line(img, self.coords[i-1], self.coords[i],(0,0,255), 3)
 
-root = 'main'
-cv2.namedWindow(root)
+            cv2.imshow(self.window_name, img)
+            if cv2.waitKey(5) & 0xFF == ord("q"):
+                break
 
-sliders = [s.make_slid(0,255,29, 'r', root)]
+        self.cap.release()
+        cv2.destroyAllWindows()
+        sys.exit(1)
 
-coords = deque(maxlen=20)
-
-while True:
-    sucess, img = cap.read()
-
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(img)
-
-    #use BGR not RGB
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-    image_height, image_width, _ = img.shape
-
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(img,hand_landmarks, connections = mp_hands.HAND_CONNECTIONS)
-            hand_pos = hand_landmarks.landmark
-            x_pos = [hand_pos[0].x, hand_pos[5].x, hand_pos[17].x]
-            y_pos = [hand_pos[0].y,hand_pos[5].y,hand_pos[17].y]                
-                
-            c = midpoint(x_pos, y_pos)
-            cx = int(c[0]*image_width)
-            cy = int(c[1]*image_height)
-            coords.appendleft([cx,cy])
-            cv2.circle(img,(cx,cy),1,(255, 0, 0),3)
-            for i in range(1, len(coords)):
-                if coords[i-1] == None or coords[i] == None:
-                    continue
-                cv2.line(img, coords[i-1], coords[i],(0,0,255), 1)
-
-    cv2.imshow(root, img)
-    if cv2.waitKey(5) & 0xFF == ord("q"):
-        break
-
-webcam.release()
-cv2.destroyAllWindows()
-
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    tracker = PaddleTracker(window_name= "tracker") 
+    tracker.track()
